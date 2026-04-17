a8a81dad31eefe5fdf65c05fcbc3607d6e7806a4
vikashjonwal2425-shiv <vikashjonwal0408@gmail.com>
2026-04-17 20:47:37 +0530

    Update result.js

--- api/result.js
+++ api/result.js
@@ -1,19 +1,21 @@
+import connectDB from './db';
+
 export default async function handler(req, res) {
-    // यहाँ हम मान लेते हैं कि ये डेटाबेस से आ रहा है
-    // असल ऐप में हम MongoDB से 'Total Bets' का सम (Sum) निकालेंगे
-    const colorBets = { red: 500, green: 800, violet: 200 };
-    const numberBets = { 0: 10, 1: 50, 2: 5, 3: 100, 4: 20, 5: 5, 6: 80, 7: 15, 8: 40, 9: 60 };
-
-    // 1. सबसे कम पैसे वाला रंग चुनना
-    const winColor = Object.keys(colorBets).reduce((a, b) => colorBets[a] < colorBets[b] ? a : b);
-
-    // 2. सबसे कम पैसे वाला नंबर चुनना
-    const winNumber = Object.keys(numberBets).reduce((a, b) => numberBets[a] < numberBets[b] ? a : b);
-
-    res.status(200).json({
-        success: true,
-        winning_color: winColor,
-        winning_number: winNumber,
-        period: Date.now()
+    const db = await connectDB();
+    
+    // डेटाबेस से आज की सारी बेट्स निकालना
+    const allBets = await db.collection("bets").find({}).toArray();
+    
+    const totals = { red: 0, green: 0, violet: 0 };
+    allBets.forEach(bet => {
+        if(totals[bet.choice] !== undefined) totals[bet.choice] += bet.amount;
     });
+
+    // सबसे कम अमाउंट वाला रंग चुनना (Winning Logic)
+    const winner = Object.keys(totals).reduce((a, b) => totals[a] < totals[b] ? a : b);
+    
+    // नंबर के लिए रैंडम (या कम वाला लॉजिक यहाँ भी लगा सकते हैं)
+    const winNum = Math.floor(Math.random() * 10);
+
+    res.status(200).json({ winning_color: winner, winning_number: winNum });
 }
